22c7492af051931253b8a8dc63a63da44db9b781
khudadad Rasikh <45727334+khRasikh@users.noreply.github.com>
2022-03-22 14:47:57 +0200

    Third changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to Frontend Web Application!
+          Welcome to my New Advanced Front Application!!!
         </a>
       </header>
     </div>
