a719ebedcf27c5a7a9e33be8f0e80bfe09205f40
khudadad Rasikh <45727334+khRasikh@users.noreply.github.com>
2022-03-22 18:15:19 +0200

    Another change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit 
+          Starting a new project is fun!
         </p>
         <a
           className="App-link"
