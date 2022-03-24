88515062cb6b7075ac51ad2bd588bee126f5d6c2
khudadad Rasikh <45727334+khRasikh@users.noreply.github.com>
2022-03-24 13:50:07 +0200

    Third changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Starting a new project is fun!
+          Starting a new TypeScript project by Rasikh 
         </p>
         <a
           className="App-link"
